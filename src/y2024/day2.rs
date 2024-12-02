3452ff33e82a58058f937924b18afc57b0678b11
Sola <dev@sola.love>
2024-12-02 13:39:41 +0800

    y2024 day2

---
+++ src/y2024/day2.rs
@@ -0,0 +1,75 @@
+use std::str::FromStr;
+
+use indoc::indoc;
+use itertools::Itertools;
+
+use crate::aoc::input;
+
+fn parse(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(u32::from_str)
+                .map(Result::unwrap)
+                .collect_vec()
+        })
+        .collect_vec()
+}
+
+fn safe(num: &[u32]) -> bool {
+    let sorted = num.is_sorted() || num.is_sorted_by(|a, b| a >= b);
+    if !sorted {
+        return false;
+    }
+    num.iter().tuple_windows().all(|(&a, &b)| {
+        let diff = a.abs_diff(b);
+        (1u32..=3).contains(&diff)
+    })
+}
+
+fn safe_p2(num: &[u32]) -> bool {
+    for i in 0..num.len() {
+        let mut num = num.to_vec();
+        num.remove(i);
+        if safe(&num) {
+            return true;
+        }
+    }
+    false
+}
+
+#[test]
+fn part1() {
+    let nums = parse(&input!());
+    let count = nums.into_iter().filter(|num| safe(num)).count();
+    dbg!(count);
+}
+
+#[test]
+fn part2() {
+    let nums = parse(&input!());
+    let count = nums.into_iter().filter(|num| safe_p2(num)).count();
+    dbg!(count);
+}
+
+const EXAMPLE1: &str = indoc! {"
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+"};
+
+#[test]
+fn test_example() {
+    let nums = parse(EXAMPLE1);
+    let count = nums.into_iter().filter(|num| safe(num)).count();
+    assert_eq!(count, 2);
+}
+
+#[test]
+fn test_p1() {
+    assert!(safe(&[7, 6, 4, 2, 1]));
+}
