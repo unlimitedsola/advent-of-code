bc40bd92c014277c30e8c8915fc6b8f4ebb16d73
Sola <dev@sola.love>
2024-12-03 13:22:11 +0800

    y2024 day3

--- src/y2024/day3.rs
+++ src/y2024/day3.rs
@@ -1,15 +1,12 @@
 use regex::Regex;
-use std::sync::LazyLock;
 
 use crate::aoc::input;
 
-static RE: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r"don't\(\)|do\(\)|mul\((\d+),(\d+)\)").unwrap());
-
 #[test]
 fn part1() {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     let input = input!();
-    let sum = RE
+    let sum = re
         .captures_iter(&input)
         .map(|cap| cap[1].parse::<u64>().unwrap() * cap[2].parse::<u64>().unwrap())
         .sum::<u64>();
@@ -18,26 +15,21 @@ fn part1() {
 
 #[test]
 fn part2() {
+    let re = Regex::new(r"don't\(\)|do\(\)|mul\((\d+),(\d+)\)").unwrap();
     let input = input!();
     let mut enable = true;
-    let sum = RE
+    let sum = re
         .captures_iter(&input)
-        .map(|cap| match &cap[0] {
-            "do()" => {
-                enable = true;
-                0
-            }
-            "don't()" => {
-                enable = false;
-                0
-            }
-            _ => {
-                if enable {
-                    cap[1].parse::<u64>().unwrap() * cap[2].parse::<u64>().unwrap()
-                } else {
-                    0
+        .map(|cap| {
+            match &cap[0] {
+                "do()" => enable = true,
+                "don't()" => enable = false,
+                _ if enable => {
+                    return cap[1].parse::<u64>().unwrap() * cap[2].parse::<u64>().unwrap()
                 }
+                _ => (),
             }
+            0
         })
         .sum::<u64>();
     dbg!(sum);
